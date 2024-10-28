acf2521810a5b6e29e99bec2c870ee390c3fb16a
Alex Hong <alexhong681@gmail.com>
2024-10-28 15:06:09 -0700

    uncomment out changes

--- background_jobs/237_background_job.py
+++ background_jobs/237_background_job.py
@@ -29,9 +29,8 @@ def prediction_background_job():
 
 
 if __name__ == '__main__':
-    prediction_background_job()
-    # # reference: https://schedule.readthedocs.io/en/stable/
-    # schedule.every(11).minutes.do(prediction_background_job)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    # reference: https://schedule.readthedocs.io/en/stable/
+    schedule.every(11).minutes.do(prediction_background_job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
